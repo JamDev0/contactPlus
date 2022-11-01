48361ff8dae8217ce10540a0ffa956db8f5045f7
JAMDev <jamdeveloper0@gmail.com>
2022-11-01 12:53:47 -0300

    Começo do desenvolvimento do Home
    Implementação do Header

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/Header";
+import { HomeContainer } from "./styles";
+
+export function Home() {
+  return (
+    <>
+      <Header />
+      <HomeContainer>
+        
+      </HomeContainer>
+    </>
+  )
+}
