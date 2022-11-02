4bc993666c006a193880231ee875833a09082b70
JAMDev <jamdeveloper0@gmail.com>
2022-11-02 15:25:41 -0300

    Desenvolvimento do contactsListReducer

---
+++ src/reducers/contactsListReducer/actions.ts
@@ -0,0 +1,20 @@
+import { contactsListState } from "./reducer"
+
+export enum actionTypes {
+  SET_CONTACTS_LIST = "SET_CONTACTS_LIST",
+  SET_CONTACTS_LIST_STATS = "SET_CONTACTS_LIST_STATS"
+}
+
+export function setContactsListAction(list: contactsListState['list']) {
+  return {
+    type: actionTypes.SET_CONTACTS_LIST,
+    payload: list
+  }
+}
+
+export function setContactsListStatusAction(status: contactsListState['status']) {
+  return {
+    type: actionTypes.SET_CONTACTS_LIST_STATS,
+    payload: status
+  }
+}
---
+++ src/reducers/contactsListReducer/reducer.ts
@@ -0,0 +1,34 @@
+import produce from "immer";
+import { actionTypes } from "./actions";
+
+interface contactsData {
+  id: number
+  name: string
+  email: string
+  phone: number
+}
+
+export interface contactsListState {
+  list: contactsData[]
+  status: 'set' | 'setting' | 'unset'
+}
+
+export function contactsListReducer(state: contactsListState, action: any) {
+  switch(action.type) {
+    case actionTypes.SET_CONTACTS_LIST: {
+      return produce(state, draft => {
+        draft.list = action.payload
+
+        draft.status = 'set'
+      })
+    }
+
+    case actionTypes.SET_CONTACTS_LIST_STATS: {
+      return produce(state, draft => {
+        draft.status = action.payload
+      })
+    }
+  }
+
+  return state
+}
