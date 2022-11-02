d7fefa75f6f491db69964cfcd5959d6d421c5541
JAMDev <jamdeveloper0@gmail.com>
2022-11-02 12:34:19 -0300

    Termino do desenvolvimento do Header

--- src/pages/Home/components/Header/index.tsx
+++ src/pages/Home/components/Header/index.tsx
@@ -1,9 +1,18 @@
-import { HeaderContainer } from "./styles";
+import { AddressBook } from "phosphor-react";
+import { AppLogoContainer, HeaderContainer, NewContactButton } from "./styles";
 
 export function  Header() {
   return (
     <HeaderContainer>
-      
+      <header>
+        <AppLogoContainer>
+          <AddressBook weight="fill" />
+
+          <span>Contact +</span>
+        </AppLogoContainer>
+
+        <NewContactButton>Novo contato</NewContactButton>
+      </header>
     </HeaderContainer>
   )
 }
--- src/pages/Home/components/Header/styles.ts
+++ src/pages/Home/components/Header/styles.ts
@@ -10,10 +10,54 @@ export const HeaderContainer = styled.div`
   padding-top: 2.5rem;
   padding-bottom: 2.5rem;
 
+  padding-inline: 3rem;
+
   background-color: ${params => params.theme.colors.gray[900]};
 
   > header {
     width: 100%;
-    max-width: ${params => params.theme.sizes.app}; 
+    max-width: ${params => params.theme.sizes.app};
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+`
+
+export const AppLogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 1rem;
+
+  font-weight: bold;
+  font-size: 1.6rem;
+  color: ${params => params.theme.colors.gray[100]};
+
+  > svg {
+    font-size: 3.125rem;
+    line-height: 0;
+
+    color: ${params => params.theme.colors.green[400]};
+  }
+`
+
+export const NewContactButton = styled.button`
+  padding: 0.75rem 1.25rem;
+
+  border-radius: 6px;
+  border: 0;
+
+  background-color: ${params => params.theme.colors.green[400]};
+
+  font-weight: bold;
+  font-size: 1rem;
+  color: ${params => params.theme.colors.white};
+
+  transition: background-color 300ms ease-in-out;
+
+  cursor: pointer;
+
+  :hover {
+    background-color: ${params => params.theme.colors.green[200]};
   }
 `
