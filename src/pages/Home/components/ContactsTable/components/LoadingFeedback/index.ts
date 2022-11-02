47ab37000879f1fa5388216835de8ecbe4b5ae21
JAMDev <jamdeveloper0@gmail.com>
2022-11-02 20:05:48 -0300

    Criação do LoadingFeedback

---
+++ src/pages/Home/components/ContactsTable/components/LoadingFeedback/index.ts
@@ -0,0 +1,22 @@
+import { CircleNotch } from "phosphor-react";
+
+import styled, { keyframes } from "styled-components";
+
+const animation = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+`
+
+export const LoadingFeedback = styled(CircleNotch)`
+  animation: ${animation};
+  animation-timing-function: cubic-bezier(0.625, -0.320, 0.375, 1.320);;
+  animation-duration: 850ms;
+  animation-iteration-count: infinite;
+
+  line-height: 0;
+`
