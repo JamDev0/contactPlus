1682d6af3afc68757d629e676362338cb995fa09
JAMDev <jamdeveloper0@gmail.com>
2022-11-02 20:06:11 -0300

    Desenvolvimento do NoContactsFeedback

---
+++ src/pages/Home/components/ContactsTable/components/NoContactsFeedback/index.tsx
@@ -0,0 +1,9 @@
+import { NoContactsFeedbackContainer } from "./styles";
+
+export function NoContactsFeedback() {
+  return (
+    <NoContactsFeedbackContainer>
+      <p role='status'>Ops! Não foi encontrado nenhum contato com esse nome</p>
+    </NoContactsFeedbackContainer>
+  )
+}
---
+++ src/pages/Home/components/ContactsTable/components/NoContactsFeedback/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const NoContactsFeedbackContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+
+  > p {
+    font-size: 1.25rem;
+  }
+`
